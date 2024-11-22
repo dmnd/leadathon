24fdeaf738380a9298bae73337f14d689a49f861
Desmond Brand <dmnd@desmondbrand.com>
2024-11-22 12:57:37 -0800

    2024 data!

--- src/data.ts
+++ src/data.ts
@@ -16,7 +16,8 @@ export function className(x: {
 }
 
 async function parseCSV(): Promise<Papa.ParseResult<Row>> {
-  const filePath = path.join(process.cwd(), "./src/2023.csv");
+  // const filePath = path.join(process.cwd(), "./src/2023.csv");
+  const filePath = path.join(process.cwd(), "./src/2024-11-22T1143.csv");
   const file = fs.readFileSync(filePath, "utf8");
   return new Promise((resolve, reject) => {
     Papa.parse<Row>(file, {
